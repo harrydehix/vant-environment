ef0afa425ff520da07b7b3b2337dd6300de53a79
harrydehix <harry.holly02@gmail.com>
2023-09-27 19:55:26 +0200

    Fix bugs

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -17,3 +17,14 @@ export const AdvancedModels = ["Pro2", "Vue"]
 export type BaudRate = 1200 | 2400 | 4800 | 9600 | 14400 | 19200
 /** Array holding all valid baud rates. */
 export const BaudRates = [ 1200, 2400, 4800, 9600, 14400, 19200]
+
+/**
+ * The rain collector's size. Is used to convert the (internally used) number of rain clicks to your configured rain unit.
+ */
+export type RainCollectorSize = "0.01in" | "0.2mm" | "0.1mm";
+
+/** Array holding all valid rain collector sizes. */
+export const RainCollectorSizes = ["0.01in", "0.2mm", "0.1mm"]
+
+/** Supported weather station models. */
+export type Model = "Pro" | "Pro2" | "Vue";
