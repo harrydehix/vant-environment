621050aa69b41367a547a797c24c44172f55af14
harrydehix <harry.holly02@gmail.com>
2023-09-27 19:51:29 +0200

    Add utils module, move some types, update docs

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,3 @@ export const RainCollectorSizes = ["0.01in", "0.2mm", "0.1mm"]
 
 /** Supported weather station models. */
 export type Model = "Pro" | "Pro2" | "Vue";
-/** Array holding all valid weather station models. */
-export const Models = ["Pro", "Pro2", "Vue"]
-
-/** Weather station models supporting new features. */
-export type AdvancedModel = "Pro2" | "Vue";
-/** Array holding all valid weather station models supporting new features. */
-export const AdvancedModels = ["Pro2", "Vue"]
-
-/** A supported baud rate. Default is `19200`. Adjustable in the vantage console. */
-export type BaudRate = 1200 | 2400 | 4800 | 9600 | 14400 | 19200
-/** Array holding all valid baud rates. */
-export const BaudRates = [ 1200, 2400, 4800, 9600, 14400, 19200]
--- src/structures/index.ts
+++ src/structures/index.ts
@@ -4,3 +4,16 @@ export { default as LOOP1 } from "./LOOP1";
 export { default as LOOP2 } from "./LOOP2";
 export { default as HighsAndLows } from "./HighsAndLows";
 export { LOOPPackageType } from "./LOOPPackageType";
+
+/** Array holding all valid weather station models. */
+export const Models = ["Pro", "Pro2", "Vue"]
+
+/** Weather station models supporting new features. */
+export type AdvancedModel = "Pro2" | "Vue";
+/** Array holding all valid weather station models supporting new features. */
+export const AdvancedModels = ["Pro2", "Vue"]
+
+/** A supported baud rate. Default is `19200`. Adjustable in the vantage console. */
+export type BaudRate = 1200 | 2400 | 4800 | 9600 | 14400 | 19200
+/** Array holding all valid baud rates. */
+export const BaudRates = [ 1200, 2400, 4800, 9600, 14400, 19200]
